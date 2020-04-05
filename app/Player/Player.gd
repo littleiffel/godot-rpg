87f4ead1adb10fdbb28a2955243b8b609f996274
Thierry Nicola <thierry.nicola@doctena.com>
2020-04-05 14:36:29 +0200

    Add script to move player

--- app/Player/Player.gd
+++ app/Player/Player.gd
@@ -1,16 +1,12 @@
 extends KinematicBody2D
 
+var velocity = Vector2.ZERO
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	
+	velocity = input_vector
+	
+	move_and_collide(velocity)
