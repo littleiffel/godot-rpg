56eeda5e054c8fbd540c0f6384609e049be13a18
Thierry Nicola <thierry.nicola@doctena.com>
2020-04-06 12:12:07 +0200

    add animation tree changes and state

--- app/Player/Player.gd
+++ app/Player/Player.gd
@@ -7,6 +7,7 @@ var velocity = Vector2.ZERO
 
 onready var animation_player = $AnimationPlayer
 onready var animation_tree = $AnimationTree
+onready var animation_state = animation_tree.get("parameters/playback")
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -15,9 +16,12 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		animation_tree.set()
+		animation_tree.set("parameters/Idle/blend_position", input_vector)
+		animation_tree.set("parameters/Run/blend_position", input_vector)
+		animation_state.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
+		animation_state.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	velocity = move_and_slide(velocity)
