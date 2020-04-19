0a2f391102c8329c5b5632ee1a79f01006b2253a
Thierry Nicola <thierry.nicola@doctena.com>
2020-04-19 16:56:32 +0200

    Adding Player Detection algo rithm to seek player

--- app/Enemies/Bat.gd
+++ app/Enemies/Bat.gd
@@ -1,19 +1,54 @@
 extends KinematicBody2D
 
-var knockback = Vector2.ZERO
+export var ACCELERATION =  300
+export var MAX_SPEED = 50
+export var FRICTION = 200
 
 const EnenmyDeathEffect = preload("res://Effects/EnemyDeathEffect.tscn")
 
 onready var animated_sprite = $AnimatedSprite
 onready var stats = $Stats
+onready var player_detection_zone = $PlayerDetectionZone
+
+enum {
+	IDLE,
+	WANDER,
+	CHASE
+}
+var knockback = Vector2.ZERO
+var velocity =  Vector2.ZERO
+
+var state = CHASE
 
 func _ready():
 	animated_sprite.frame = randi()%5
 	animated_sprite.speed_scale = rand_range(0.7,1.3)
 
 func _physics_process(delta):
-	knockback = knockback.move_toward(Vector2.ZERO, 200 * delta)
+	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
 	knockback = move_and_slide(knockback)
+	
+	match state:
+		IDLE:
+			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+			seek_player()
+		WANDER:
+			pass
+		CHASE:
+			var player = player_detection_zone.player
+			if player != null:
+				var direction = (player.global_position - global_position).normalized()
+				velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+			animated_sprite.flip_h = velocity.x < 0
+			seek_player()
+	velocity = move_and_slide(velocity)
+			
+func seek_player():
+	if player_detection_zone.can_see_player():
+		state = CHASE
+	else:
+		state = IDLE
+		
 
 func _on_HurtBox_area_entered(area):
 	stats.health -= area.damage
--- app/Hurt and Hitboxes/Hurtbox.gd
+++ app/Hurt and Hitboxes/Hurtbox.gd
@@ -1,13 +1,12 @@
 extends Area2D
 
+export(bool) var  show_hit = true
+
 const hit_effet = preload("res://Effects/HitEffect.tscn")
 
-
-
-
-
 func _on_HurtBox_area_entered(_area):
-	var effect = hit_effet.instance()
-	var main = get_tree().current_scene
-	main.add_child(effect)
-	effect.global_position = global_position
+	if show_hit:
+		var effect = hit_effet.instance()
+		var main = get_tree().current_scene
+		main.add_child(effect)
+		effect.global_position = global_position
